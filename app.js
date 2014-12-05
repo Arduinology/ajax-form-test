3001b32b27660e29658603b1fae06505eeccd96a
Heath Naylor <HeathNaylor@gmail.com>
2014-12-05 11:30:46 -0700

    Simplify code base

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-$(document).ready(function() {
+(function() {
+	//ajax-form
+	var form = document.getElementsByTagName('form')[0];
 
-	window.addEventListener('polymer-ready', function(e) {
-			//ajax-form
-			var form = document.getElementsByTagName('form')[0];
-			$('#submit-form').on('click', function() {
-			  document.forms[0].submit(); 
-			});
+	document.getElementById('submit-form').addEventListener('click', function() {
+      document.forms[0].submit(); 
+   	});
 
-			//If the form is successfully submitted
-			if(document.contains(form)) {
-				form.addEventListener('submitted', function(e) {
-		    		console.log("here");
-			        if (event.detail.status > 299) {
-			            // submit has failed
-			        }
-			    });
-			}
-	});
-});
+	//If the form is successfully submitted
+	if(document.contains(form)) {
+		form.addEventListener('submitted', function(e) {
+    		console.log("here");
+	        if (event.detail.status > 299) {
+	            // submit has failed
+	        }
+	    });
+	}
+}());
--- testing.php
+++ testing.php
@@ -7,34 +7,19 @@
     <head>
         <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script src="/bower_components/webcomponentsjs/webcomponents.js"></script>
-        <link rel="import" href="/bower_components/core-style/core-style.html">
         <link rel="import" href="/bower_components/ajax-form/ajax-form.html">
-        <link href="/bower_components/core-header-panel/core-header-panel.html" rel="import">
-        <link href="/bower_components/core-toolbar/core-toolbar.html" rel="import">
-        <link href="/bower_components/core-drawer-panel/core-drawer-panel.html" rel="import">
-        <link href="/bower_components/core-collapse/core-collapse.html" rel="import">
-        <link href="/bower_components/core-menu/core-menu.html" rel="import">
         <link rel="import" href="/bower_components/paper-button/paper-button.html">
-        <link href="/bower_components/core-icons/core-icons.html" rel="import">
-        <link href="/bower_components/paper-icon-button/paper-icon-button.html" rel="import">
-        <link href="/bower_components/paper-dropdown/paper-dropdown.html" rel="import">
-        <link href="/bower_components/paper-item/paper-item.html" rel="import">
-        <link href="/bower_components/paper-shadow/paper-shadow.html" rel="import">
-        <link rel="import" href="/bower_components/paper-menu-button/paper-menu-button.html">
+        <link rel="import" href="/bower_components/paper-shadow/paper-shadow.html">
         <link rel="import" href="/bower_components/paper-input/paper-input.html">
-        <link rel="import" href="/bower_components/paper-input/paper-autogrow-textarea.html">
-        <link rel="import" href="/bower_components/form/form.html">
-        <link rel="import" href="/bower_tracked/paper-dropdown-menu-header/paper-dropdown-menu-header.html">
-        <link rel="import" href="/bower_components/paper-dropdown-menu/paper-dropdown-menu.html">
-        <script src="/app.js"></script>
     </head>
     <body>
-        <form method="POST" action="/testing.php" is="ajax-form">
+        <form method="POST" action="testing.php" is="ajax-form">
             <input name="testing"></input>
             <paper-input name="name" label="Name" floatingLabel="true"></paper-input>
             <paper-input name="description" label="Description" floatingLabel="true"></paper-input>
             <paper-input name="website" label="Web Address" floatingLabel="true"></paper-input>
             <paper-button raised id="submit-form" class="button-blue" type="submit">Submit</paper-button>
         </form>
+        <script src="/app.js"></script>
     </body>
 </html>
